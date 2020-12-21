08285d7a2e39c4231115f76fbc49c44d326007a8
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-21 12:09:24 +0200

    Add the steps first solution

--- steps/index.js
+++ steps/index.js
@@ -18,10 +18,16 @@
 //       '####'
 
 function steps(n) {
-    for (let i = 1; i <= n; i++) {
-        for (let j = 0; j < i; j++) {
-            return "# /t";
+    for (let i = 0; i < n; i++) {
+        let stair = "";
+        for (let j = 0; j < n; j++) {
+            if (j <= i) {
+                stair += "#";
+            } else {
+                stair += " ";
+            }
         }
+        console.log(stair);
     }
 }
 
