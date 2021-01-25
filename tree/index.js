26e02fb3c507d627005e5dccf08d591350b90d91
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-25 22:52:15 +0200

    remove specific data from the  tree

--- tree/index.js
+++ tree/index.js
@@ -20,6 +20,11 @@ class Node {
         const node = new Node(data);
         this.children.push(node);
     }
+    remove(data) {
+        this.children = this.children.filter((node) => {
+            return node.data !== data;
+        });
+    }
 }
 
 class Tree {}
